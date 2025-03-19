fb438b2691e4ef27fdb4814f1e772a9c7a8995c4
Aatif Raza <razaaatif25@gmail.com>
2025-03-19 14:03:30 +0530

    Update and rename stack_2.java to Stack_2.java

--- Stack_2.java
+++ Stack_2.java
@@ -9,13 +9,13 @@ public class stack_2 {
         stack.push("Apple");
         stack.push("Banana");
         stack.push("Mango");
-        stack.push("Kela");
+        stack.push("Orange");
         System.out.println("Stack after pushing: " + stack); // Output: [Apple, Banana, Mango, Kela]
         System.out.println("Stack size: " + stack.size());
 
         // Removing the top item from the stack (pop)
         String lastItem = stack.pop();
-        System.out.println("Popped item: " + lastItem); // Output: Kela
+        System.out.println("Popped item: " + lastItem); // Output: Orange
         System.out.println("Stack after popping: " + stack); // Output: [Apple, Banana, Mango]
         System.out.println("Stack size: " + stack.size());
         // Checking if the stack is empty after popping
